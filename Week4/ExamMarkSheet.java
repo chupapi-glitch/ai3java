0711dee8f82dbe8a0142a21dbc9ce9b5fff02517
JB Malla <weeeeweeeeweeeeweeeew@gmail.com>
2025-11-26 13:02:44 +0545

    pushing

---
+++ Week4/ExamMarkSheet.java
@@ -0,0 +1,32 @@
+package Week4;
+import java.util.Scanner;
+
+
+/**
+ * Write a description of class Example1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ExamMarkSheet
+
+{
+    public static void main(String[]arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int percentage=sc.nextInt();
+        if ((percentage<=100) && (percentage>=90))
+        {
+            System.out.println("Distinction");
+        }
+        else if ((percentage>=70) && (percentage<90))
+        {
+            System.out.println("First Dicision");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+    }
+}
---
+++ chupapi/IfLadder.java
@@ -0,0 +1,13 @@
+package chupapi;
+
+
+/**
+ * Write a description of class IfLadder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class IfLadder
+{
+    
+}
